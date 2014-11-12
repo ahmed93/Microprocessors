396a5b3229a3aee086aa396a9ab4891a42b1ea7e
MohabGhanim <mohab.ghanim@gmail.com>
2014-11-12 21:54:48 +0700

    Singleton classes

--- src/simulator/Memory.java
+++ src/simulator/Memory.java
@@ -6,11 +6,19 @@ public class Memory {
 
 	final int SIZE = 32768;
 	Word[] cells = new Word[SIZE];
+	private static Memory memory_instance;
 
 	int percentage;
 	int d = percentage / 100 * SIZE; // Data Index
 	int i; // Instruction Index
-
+	
+	private Memory(){}
+	
+	public Memory get_instance(){
+		if (memory_instance == null)
+			memory_instance = new Memory();
+		return memory_instance;
+	}
 	public void store_date(String data, int address){}
 
 	public void store_instruction(Instruction instruction, int address){}	
--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -3,12 +3,21 @@ import Abstracts.Cache;
 
 public class Simulator {
 	
+	Simulator simulator_instance;
 	Cache[] caches;
 	Memory memory;
 	static int pc;
 	Register[] registers;
 	
-	public Simulator(){
+	private Simulator(){}
+	
+	public Simulator get_instance(){
+		if (simulator_instance == null)
+			simulator_instance = new Simulator();
+		return simulator_instance;
+	}
+	
+	public void Initialize(){
 		// Initialize Cache
 		// Prompt user to enter instructions, and starting address
 		// Use Instruction factory to create instructions
