26ff40a33c46cc2c52236c9e6f5e33a805cf2dbe
Ahmed Moataz <a7mad.mo3taz@gmail.com>
2014-11-15 13:21:27 +0200

    Adding create_instruction

--- src/factories/InstructionFactory.java
+++ src/factories/InstructionFactory.java
@@ -1,12 +1,72 @@
 package factories;
 
+import instructions.*;
 import simulator.Simulator;
 import Abstracts.Instruction;
 
 public class InstructionFactory {
 
-	public static Instruction create_instruction(String instruction, Simulator simulator) {
-		// Returns an object that corresponds to the instruction specified in the parameter.
+	public static Instruction create_instruction(String instruction,
+			Simulator simulator) {
+		String[] instructionArray = instruction.split("(?: |, )");
+		switch (instructionArray[0]) {
+		case "LW":
+			return new LW(simulator, instruction, instruction,
+					simulator.getRegister(instructionArray[1]),
+					simulator.getRegister(instructionArray[2]),
+					Integer.parseInt(instructionArray[3]));
+		case "SW":
+			return new SW(simulator, instruction, instruction,
+					simulator.getRegister(instructionArray[1]),
+					simulator.getRegister(instructionArray[2]),
+					Integer.parseInt(instructionArray[3]));
+		case "JMP":
+			return new JMP(simulator, instruction, instruction,
+					simulator.getRegister(instructionArray[1]),
+					Integer.parseInt(instructionArray[2]));
+		case "BEQ":
+			return new BEQ(simulator, instruction, instruction,
+					simulator.getRegister(instructionArray[1]),
+					simulator.getRegister(instructionArray[2]),
+					Integer.parseInt(instructionArray[3]));
+		case "JALR":
+			return new JALR(simulator, instruction, instruction,
+					simulator.getRegister(instructionArray[1]),
+					simulator.getRegister(instructionArray[2]));
+//		case "RET":
+//			return new RET(simulator, instruction, instruction,
+//					simulator.getRegister(instructionArray[1]));
+		case "ADD":
+			return new ADD(simulator, instruction, instruction,
+					simulator.getRegister(instructionArray[1]),
+					simulator.getRegister(instructionArray[2]),
+					simulator.getRegister(instructionArray[3]));
+		case "SUB":
+			return new SUB(simulator, instruction, instruction,
+					simulator.getRegister(instructionArray[1]),
+					simulator.getRegister(instructionArray[2]),
+					simulator.getRegister(instructionArray[3]));
+		case "ADDI":
+			return new ADDI(simulator, instruction, instruction,
+					simulator.getRegister(instructionArray[1]),
+					simulator.getRegister(instructionArray[2]),
+					Integer.parseInt(instructionArray[3]));
+		case "NAND":
+			return new NAND(simulator, instruction, instruction,
+					simulator.getRegister(instructionArray[1]),
+					simulator.getRegister(instructionArray[2]),
+					simulator.getRegister(instructionArray[3]));
+		case "MUL":
+			return new MUL(simulator, instruction, instruction,
+					simulator.getRegister(instructionArray[1]),
+					simulator.getRegister(instructionArray[2]),
+					simulator.getRegister(instructionArray[3]));
+
+		}
+
+		// Returns an object that corresponds to the instruction specified in
+		// the parameter.
+
 		return null;
 	}
 
