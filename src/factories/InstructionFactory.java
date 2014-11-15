f6114c2d4592d7344654c073c899e017e824d346
mohamedsaeed93 <mohamed.elsaeed93@gmail.com>
2014-11-15 19:33:11 +0200

    Fixing the instruction factory class

--- src/factories/InstructionFactory.java
+++ src/factories/InstructionFactory.java
@@ -11,53 +11,53 @@ public class InstructionFactory {
 		String[] instructionArray = instruction.split("(?: |, )");
 		switch (instructionArray[0]) {
 		case "LW":
-			return new LW(simulator, instruction, instruction,
+			return new LW(simulator, instruction,
 					simulator.getRegister(instructionArray[1]),
 					simulator.getRegister(instructionArray[2]),
 					Integer.parseInt(instructionArray[3]));
 		case "SW":
-			return new SW(simulator, instruction, instruction,
+			return new SW(simulator, instruction,
 					simulator.getRegister(instructionArray[1]),
 					simulator.getRegister(instructionArray[2]),
 					Integer.parseInt(instructionArray[3]));
 		case "JMP":
-			return new JMP(simulator, instruction, instruction,
+			return new JMP(simulator, instruction,
 					simulator.getRegister(instructionArray[1]),
 					Integer.parseInt(instructionArray[2]));
 		case "BEQ":
-			return new BEQ(simulator, instruction, instruction,
+			return new BEQ(simulator, instruction,
 					simulator.getRegister(instructionArray[1]),
 					simulator.getRegister(instructionArray[2]),
 					Integer.parseInt(instructionArray[3]));
 		case "JALR":
-			return new JALR(simulator, instruction, instruction,
+			return new JALR(simulator, instruction,
 					simulator.getRegister(instructionArray[1]),
 					simulator.getRegister(instructionArray[2]));
-//		case "RET":
-//			return new RET(simulator, instruction, instruction,
-//					simulator.getRegister(instructionArray[1]));
+		case "RET":
+			return new RET(simulator, instruction,
+					simulator.getRegister(instructionArray[1]));
 		case "ADD":
-			return new ADD(simulator, instruction, instruction,
+			return new ADD(simulator, instruction,
 					simulator.getRegister(instructionArray[1]),
 					simulator.getRegister(instructionArray[2]),
 					simulator.getRegister(instructionArray[3]));
 		case "SUB":
-			return new SUB(simulator, instruction, instruction,
+			return new SUB(simulator, instruction,
 					simulator.getRegister(instructionArray[1]),
 					simulator.getRegister(instructionArray[2]),
 					simulator.getRegister(instructionArray[3]));
 		case "ADDI":
-			return new ADDI(simulator, instruction, instruction,
+			return new ADDI(simulator, instruction,
 					simulator.getRegister(instructionArray[1]),
 					simulator.getRegister(instructionArray[2]),
 					Integer.parseInt(instructionArray[3]));
 		case "NAND":
-			return new NAND(simulator, instruction, instruction,
+			return new NAND(simulator, instruction,
 					simulator.getRegister(instructionArray[1]),
 					simulator.getRegister(instructionArray[2]),
 					simulator.getRegister(instructionArray[3]));
 		case "MUL":
-			return new MUL(simulator, instruction, instruction,
+			return new MUL(simulator, instruction,
 					simulator.getRegister(instructionArray[1]),
 					simulator.getRegister(instructionArray[2]),
 					simulator.getRegister(instructionArray[3]));
